ebc97c8815b829629eaa984beec0fd42e3c75ac1
Peter <peterpfleiderer@gmx.de>
2017-04-21 16:59:46 +0000

    remove import

--- app/settings.py
+++ app/settings.py
@@ -24,14 +24,8 @@
 
 import sys,glob,os,pickle
 import numpy as np
-from netCDF4 import Dataset,netcdftime,num2date
 import pandas as pd
 
-sys.path.append('/Users/peterpfleiderer/Documents/Projects/country_analysis/country_analysis_scripts/')
-try:del sys.modules['country_analysis_obj'] 
-except:pass
-from country_analysis_obj import country_analysis,plot_map,new_data_object
-os.chdir('/Users/peterpfleiderer/Documents/Projects/projection_sharing/app/')
 
 import matplotlib
 matplotlib.use('Agg')
