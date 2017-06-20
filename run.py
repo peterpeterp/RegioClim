beec05aa14d1a8fb8c52bead78fa9bf0537727a9
Peter <peterpfleiderer@gmx.de>
2017-06-20 12:30:18 +0200

    change run.py

--- run.py
+++ run.py
@@ -3,9 +3,9 @@ from app import app
 
 #app = Flask(__name__)
 ##Bootstrap(app)
-app.config.from_object('config')
+#app.config.from_object('config')
 #app.debug = True
 
 if __name__ == "__main__":
-    app.debug = True
+    #app.debug = True
     app.run()
