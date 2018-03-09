35629a1facfd920fb404af358313513b15399f90
Peter <peterpfleiderer@gmx.de>
2018-03-09 17:20:56 +0000

    comments finaly changed away from wacalc

--- app/forms.py
+++ app/forms.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2014 Matthias Mengel and Carl-Friedrich Schleussner
+# Copyright (C) 2017 Peter Pfleiderer
 #
-# This file is part of wacalc.
+# This file is part of regioclim.
 #
-# wacalc is free software; you can redistribute it and/or modify it under the
+# regioclim is free software; you can redistribute it and/or modify it under the
 # terms of the GNU General Public License as published by the Free Software
 # Foundation; either version 3 of the License, or (at your option) any later
 # version.
 #
-# wacalc is distributed in the hope that it will be useful, but WITHOUT ANY
+# regioclim is distributed in the hope that it will be useful, but WITHOUT ANY
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 # FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
 # details.
 #
 # You should have received a copy of the GNU General Public License
-# along with wacalc; if not, write to the Free Software
+# along with regioclim; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+
 
 
 from wtforms import RadioField, SelectMultipleField, TextField, IntegerField, SelectField, StringField
