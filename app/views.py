abdaf1916d85b46c3635e77109c27196197f434e
peterpeterp <peterpfleiderer@gmx.de>
2019-06-26 11:24:20 +0200

    debug download

--- app/views.py
+++ app/views.py
@@ -845,6 +845,8 @@ def prepare_for_download(plot_request):
     if request_type=='get_data':
         curretn_path=os.getcwd()
         os.chdir('../country_analysis/data/'+s['country']+'/')
+        print('_________________________')
+        os.system('ls')
         os.system('tar -vzcf ../'+s['country']+'_'+s['indicator']+'.tar.gz area_average/*-'+s['indicator']+'_* raw/*_'+s['indicator']+'_*')
         os.chdir(curretn_path)
         filename=s['country']+'_'+s['indicator']+'.tar.gz'
